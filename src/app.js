7326547f22862852dd42ee204c981762497bec59
DaniVera-tech <daniss.vera@gmail.com>
2025-01-27 08:22:14 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
+  
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let numWho = Math.floor(Math.random() * who.length);
+  let numAction = Math.floor(Math.random() * action.length);
+  let numWhat = Math.floor(Math.random() * what.length);
+  let numWhen = Math.floor(Math.random() * when.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+
+  document.querySelector('#excuse').innerHTML = who[numWho] + " " + action[numAction] + " " + what[numWhat] + " " + when[numWhen]
+  
